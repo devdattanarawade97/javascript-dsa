346fd864727ffa6e9f1f4da4fc76f22e8d1d52f6
devdatta <devdattanarawade97@gmail.com>
2025-08-24 17:24:34 +0530

    feat: add substract function

---
+++ src/functions/substract.js
@@ -0,0 +1,11 @@
+/**
+ * Subtracts two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number (subtrahend).
+ * @returns {number} The result of subtracting b from a.
+ */
+function substract(a, b) {
+  return a - b;
+}
+
+module.exports = substract;
