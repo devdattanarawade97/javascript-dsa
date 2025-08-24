39c4f6cf9b7817b1be49e6049f349f0ca3fe7898
devdatta <devdattanarawade97@gmail.com>
2025-08-24 10:16:19 +0530

    feat: Add sum function for two numbers

---
+++ src/functions/sum.js
@@ -0,0 +1,9 @@
+/**
+ * Calculates the sum of two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} The sum of a and b.
+ */
+export function sum(a, b) {
+  return a + b;
+}
