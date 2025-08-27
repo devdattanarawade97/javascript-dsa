402a6e036762a13e7bea0bb60cac9283f46e5fd6
devdatta <devdattanarawade97@gmail.com>
2025-08-27 22:30:08 +0530

    feat: Enhance isPrime.js with findPrimeNumbers for array processing

--- src/functions/isPrime.js
+++ src/functions/isPrime.js
@@ -1,30 +1,37 @@
 /**
- * Checks if a given number is a prime number.
+ * Checks if a single number is prime.
  * A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
  *
- * @param {number} num The number to check for primality.
+ * @param {number} num The number to check.
  * @returns {boolean} True if the number is prime, false otherwise.
  */
-function isPrime(num) {
+export function isPrime(num) {
   if (num <= 1) {
-    return false; // Numbers less than or equal to 1 are not prime
+    return false;
   }
-  if (num <= 3) {
-    return true; // 2 and 3 are prime numbers
+  if (num <= 3) { // 2 and 3 are prime
+    return true;
   }
   if (num % 2 === 0 || num % 3 === 0) {
-    return false; // Multiples of 2 or 3 are not prime (except 2 and 3 themselves, which are handled above)
+    return false;
   }
-
-  // Check for prime numbers from 5 onwards
-  // All primes greater than 3 can be expressed in the form 6k ± 1
   for (let i = 5; i * i <= num; i = i + 6) {
     if (num % i === 0 || num % (i + 2) === 0) {
       return false;
     }
   }
-
   return true;
 }
 
-module.exports = isPrime; // Export the function for use in other modules
+/**
+ * Finds all prime numbers within an array of numbers.
+ *
+ * @param {number[]} numbers An array of numbers to check for primality.
+ * @returns {number[]} An array containing only the prime numbers from the input array.
+ */
+export function findPrimeNumbers(numbers) {
+  if (!Array.isArray(numbers)) {
+    throw new Error("Input must be an array of numbers.");
+  }
+  return numbers.filter(num => typeof num === 'number' && isPrime(num));
+}
